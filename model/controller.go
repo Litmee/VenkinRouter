8140242482af8b4d53723adb37c85c658de919f4
litmee <1970432@qq.com>
2022-07-20 16:55:00 +0800

    Controller model init

---
+++ model/controller.go
@@ -0,0 +1,21 @@
+package model
+
+import "net/http"
+
+// Controller Control Layer Interface Model
+// Used to define the type of request
+// Get()      Http GET Request
+// Post()     Http POST Request
+// Put()      Http PUT Request
+// Delete()   Http DELETE Request
+// Other()    Http Other Requests
+type Controller interface {
+	Get()
+	Post()
+	Put()
+	Delete()
+	Other()
+	initReq(r *http.Request)
+	initRsp(w *http.ResponseWriter)
+	judgeMethod(m string, c Controller)
+}
