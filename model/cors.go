fadfbb8e3efdbb58b0b999dd7fe152cf282e2202
litmee <1970432@qq.com>
2022-07-21 18:13:38 +0800

    cors.go init

---
+++ model/cors.go
@@ -0,0 +1,31 @@
+package model
+
+import "net/http"
+
+var (
+	// AllowOrigin Scopes allowed across domains
+	allowOrigin string
+	// AllowMethods Methods that allow cross-domain
+	allowMethods string
+	// AllowHeaders Allow cross-domain headers
+	allowHeaders string
+	// AllowOriginBool Whether a scope has been configured to allow cross-domain
+	allowOriginBool bool
+	// AllowMethodsBool Has the method configured to allow cross-domain
+	allowMethodsBool bool
+	// AllowHeadersBool Whether the header information that allows cross-domain has been configured
+	allowHeadersBool bool
+)
+
+// Set up cross-domain utility functions
+func setCorsFunc(w *http.ResponseWriter) {
+	if allowOriginBool {
+		(*w).Header().Set("Access-Control-Allow-Origin", allowOrigin)
+	}
+	if allowMethodsBool {
+		(*w).Header().Set("Access-Control-Allow-Methods", allowMethods)
+	}
+	if allowHeadersBool {
+		(*w).Header().Set("Access-Control-Allow-Headers", allowHeaders)
+	}
+}
